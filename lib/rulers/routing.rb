a2e259f81ff8e4ec4c2d8a768358c7b19329ad67
SupahNickie <nicholascase@live.com>
2013-11-04 16:08:14 -0800

    Checking in (so I can switch computers)

---
+++ lib/rulers/routing.rb
@@ -0,0 +1,13 @@
+module Rulers
+  class Application
+    def get_controller_and_action(env)
+      _, cont, action, after =
+        env["PATH_INFO"].split('/', 4)
+      cont = cont.capitalize # As in "People"
+      cont += "Controller" # As in "PeopleController"
+
+      [Object.const_get(cont), action]
+    end
+  end
+
+end
